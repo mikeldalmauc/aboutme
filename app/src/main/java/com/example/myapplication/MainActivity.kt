6291f45e341e93b975eff10c0a4bffebca514dd3
mikeldalmauc <mikeldalmauc@gmail.com>
2024-09-16 13:59:19 +0200

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Content(
+
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Content() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0.1f, 0.1f, 0.1f, 0.95f))
+    ){
+        Image(painter = painterResource(id = R.drawable.profile_picture)
+            , contentDescription = "Personal Profile Picture"
+            , alignment = Alignment.Center
+            , modifier = Modifier
+                .size(220.dp)
+                .clip(CircleShape)
+                .align(Alignment.CenterHorizontally)
+                .border(border = BorderStroke(6.dp,Color.White)
+                    , shape = CircleShape)
+        )
+        Text(
+            text = "Mikel Dalmau",
+            fontSize = 24.sp,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContentPreview() {
+    MyApplicationTheme {
+        Content()
+    }
+}
