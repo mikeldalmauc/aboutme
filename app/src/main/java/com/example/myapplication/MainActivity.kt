33423c3626737d616abf6b69b3d41782b0c74829
mikeldalmauc <mikeldalmauc@gmail.com>
2024-09-17 07:43:30 +0200

    some changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,27 +13,35 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -52,20 +60,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Content() {
-    LazyColumn(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color(0.1f, 0.1f, 0.1f, 0.95f))
-            .padding(vertical = 20.dp)
-    ){
-        item {
-            Profile()
-            About()
-            Social()
+    ) {
+        // Imagen de fondo
+        Image(
+            painter = painterResource(id = R.drawable.fondo),
+            contentDescription = "Fondo",
+            modifier = Modifier.fillMaxSize(),
+            colorFilter = ColorFilter.lighting(Color
+            ),
+            contentScale = ContentScale.FillHeight // Ajusta la imagen para que ocupe todo el tamaño del contenedor
+        )
+
+        // LazyColumn con fondo semi-transparente
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0.1f, 0.1f, 0.1f, 0.9f)) // Fondo semi-transparente
+                .padding(vertical = 20.dp)
+        ) {
+            item {
+                Profile()
+                About()
+                Social()
+            }
         }
     }
-
 }
+
 
 @Composable
 fun Profile() {
@@ -81,6 +105,8 @@ fun Profile() {
                 .clip(CircleShape)
                 .border(BorderStroke(6.dp, Color.White), shape = CircleShape)
         )
+        HorizontalDivider(color = Color.DarkGray, thickness = 1.dp, modifier = Modifier
+            .padding(vertical = 10.dp))
         Text(
             text = "Mikel Dalmau",
             fontSize = 24.sp,
@@ -90,6 +116,7 @@ fun Profile() {
                 .fillMaxWidth()
                 .padding(vertical = 10.dp)
         )
+        HorizontalDivider(color = Color.DarkGray, thickness = 1.dp)
         Text(
             text = "I am a former Software Engineer now teaching the way of the keyboard to young guns " +
                     "with a passion for sports, all kinds of food, and painting.",
@@ -136,7 +163,10 @@ fun AboutPreview(){
 
 @Composable
 fun Subject(@DrawableRes icon: Int, title: String, body: String?) {
-    Row (modifier = Modifier.fillMaxWidth().height(100.dp).padding(vertical = 5.dp, horizontal = 16.dp)
+    Row (modifier = Modifier.fillMaxWidth()
+            .height(100.dp)
+            .padding(vertical = 5.dp, horizontal = 16.dp)
+            .border(BorderStroke(Dp.Hairline, Color.White), shape = RoundedCornerShape(16.dp))
         , horizontalArrangement = Arrangement.spacedBy(16.dp)
     ){
         Image(
@@ -144,7 +174,8 @@ fun Subject(@DrawableRes icon: Int, title: String, body: String?) {
             contentDescription = "Github Icon",
             modifier = Modifier
                 .size(60.dp) // Ajusta el tamaño del ícono según el alto que desees
-                .align(Alignment.CenterVertically), // Alinea verticalmente el ícon
+                .align(Alignment.CenterVertically)// Alinea verticalmente el ícon
+                .offset(x = 5.dp, y = 0.dp), // Ajusta la posición del ícono
             colorFilter = ColorFilter.lighting(Color.White, Color.White)
         )
         Column (verticalArrangement = Arrangement.SpaceEvenly,
