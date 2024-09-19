41342c6334a7da53956c3211ea792c54c1d57006
mikeldalmauc <mikeldalmauc@gmail.com>
2024-09-19 07:38:17 +0200

    scaffold

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.BlurMaskFilter
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,10 +13,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,19 +27,40 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -45,6 +70,16 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.graphics.PathParser
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,28 +87,183 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Content()
+                Scaffold (
+                    modifier = Modifier,
+                    topBar = {
+                        TopBar()
+                    },
+                    bottomBar = {
+                        BottomBar()
+                    },
+                    floatingActionButton = {
+                        LikeActionButtonWithShadow(onClick = { println("Like") })
+                    }
+                ) {
+                    innerPadding ->
+                        Content(innerPadding)
+                }
             }
         }
     }
 }
 
 @Composable
-fun Content() {
-    Box(
+fun TopBar() {
+    val context = LocalContext.current // Access the current context
+
+    Row(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth() // Barra que ocupa todo el ancho
+            .background(Color(94, 94, 94, 37)) // Color de fondo personalizado
+            .padding(16.dp) // Padding de la barra
+            .statusBarsPadding() // Add padding to avoid overlapping with system bar
+        , verticalAlignment = Alignment.CenterVertically, // Centrar el contenido verticalmente
+        horizontalArrangement = Arrangement.SpaceBetween // Distribuye los elementos equitativamente
     ) {
-        // Imagen de fondo
-        Image(
-            painter = painterResource(id = R.drawable.fondo),
-            contentDescription = "Fondo",
-            modifier = Modifier.fillMaxSize(),
-            colorFilter = ColorFilter.lighting(Color
-            ),
-            contentScale = ContentScale.FillHeight // Ajusta la imagen para que ocupe todo el tamaño del contenedor
+        // Título de la app
+        Text(
+            text = "About Me",
+            color = Color.DarkGray,
+            fontSize = 20.sp
         )
 
+        // Íconos o acciones en la parte derecha
+        IconButton(onClick = {
+            // Create an intent to share a link
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_SUBJECT, "Check out my website!")
+                putExtra(Intent.EXTRA_TEXT, "http://www.mikeldalmauc.com") // Your website URL
+            }
+
+            // Start the sharing activity
+            startActivity(context, Intent.createChooser(shareIntent, "Share via"), null)
+        }) {
+            Icon(
+                imageVector = Icons.Default.Share,
+                contentDescription = "Share",
+                tint = Color.Black
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
+}
+
+@Composable
+fun BottomBar() {
+    val context = LocalContext.current
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth() // Ocupa todo el ancho
+            .height(56.dp) // Altura de la barra
+            .background(Color(94, 94, 94, 37)) // Color de fondo personalizado
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically, // Centra el contenido verticalmente
+        horizontalArrangement = Arrangement.End // Alinea el contenido a la derecha
+    ) {
+        // Ícono para enviar un correo
+        IconButton(onClick = {
+            // Intent para abrir la aplicación de correo
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // Solo abre aplicaciones de correo
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("mikeldalmauc@gmail.com")) // Email destinatario
+                putExtra(Intent.EXTRA_SUBJECT, "He visitado tu perfil!") // Asunto del correo
+            }
+
+            // Verifica si hay alguna aplicación que pueda manejar el intent
+            if (intent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(intent) // Inicia la aplicación de correo
+            }
+        }) {
+            Icon(
+                imageVector = Icons.Default.Email, // Ícono de correo
+                contentDescription = "Enviar email",
+                tint = Color.DarkGray, // Color del ícono
+                modifier = Modifier
+                    .size(35.dp) // Padding del ícono
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BottomBarPreview() {
+    BottomBar()
+}
+
+@Composable
+fun LikeActionButtonWithShadow(onClick: () -> Unit) {
+    Image(
+        painter = painterResource(id = R.drawable.baseline_favorite_24),
+        contentDescription = "Favorite",
+        modifier = Modifier
+            .size(80.dp) // Ajusta el tamaño según necesites
+            .clickable(onClick = onClick)
+            .drawBehind {
+                // Crear el Path basado en las coordenadas del pathData
+                val heartPath = Path().apply {
+                    moveTo(12f, 21.35f)
+                    lineTo(10.55f, 20.03f)
+                    cubicTo(5.4f, 15.36f, 2f, 12.28f, 2f, 8.5f)
+                    cubicTo(2f, 5.42f, 4.42f, 3f, 7.5f, 3f)
+                    cubicTo(9.24f, 3f, 10.91f, 3.81f, 12f, 5.09f)
+                    cubicTo(13.09f, 3.81f, 14.76f, 3f, 16.5f, 3f)
+                    cubicTo(19.58f, 3f, 22f, 5.42f, 22f, 8.5f)
+                    cubicTo(22f, 12.28f, 18.6f, 15.36f, 13.45f, 20.04f)
+                    lineTo(12f, 21.35f)
+                    close()
+                }
+
+                // Escalar el Path para que coincida con el tamaño del ícono
+                val scaleFactor = size.width / 24f // Escalar de acuerdo al tamaño del canvas
+                scale(scale = scaleFactor, pivot = Offset.Zero) {
+                    // Dibujar la sombra
+                    val shadowColor = Color.Black.copy(alpha = 0.5f)
+                    val offset = Offset(1.5f, 1.5f) // Ajusta la posición de la sombra
+                    // Usa `drawIntoCanvas` para aplicar el blur a la sombra
+                    drawIntoCanvas { canvas ->
+                        val paint = android.graphics
+                            .Paint()
+                            .apply {
+                                color = android.graphics.Color.BLACK
+                                alpha = 128 // Nivel de transparencia
+                                maskFilter = BlurMaskFilter(
+                                    0.5f,
+                                    BlurMaskFilter.Blur.NORMAL
+                                ) // Aplicar desenfoque
+                            }
+
+                        // Dibujar el Path con sombra y desenfoque
+                        canvas.nativeCanvas.translate(offset.x, offset.y)
+                        canvas.nativeCanvas.drawPath(heartPath.asAndroidPath(), paint)
+                    }
+                }
+            }
+    )
+
+}
+
+@Composable
+@Preview(showBackground = false)
+fun LikeActionButtonPreview() {
+    // FloatingActionButton
+    LikeActionButtonWithShadow(onClick = { println("Like") })
+}
+
+@Composable
+fun Content(paddingValues: PaddingValues) {
+    Box(
+        modifier = Modifier
+            .padding(paddingValues = paddingValues)
+            .fillMaxSize()
+    ) {
         // LazyColumn con fondo semi-transparente
         LazyColumn(
             modifier = Modifier
@@ -135,14 +325,14 @@ fun Profile() {
 @Preview(showBackground = true)
 @Composable
 fun ContentPreview() {
-    MyApplicationTheme {
-        Content()
-    }
+    Content(paddingValues = PaddingValues(5.dp))
 }
 
 @Composable
 fun About(){
-    Column (modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp)
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 10.dp)
         , verticalArrangement = Arrangement.SpaceEvenly
     )
     { // Row for the social media icons
@@ -156,17 +346,16 @@ fun About(){
 @Preview(showBackground = false)
 @Composable
 fun AboutPreview(){
-    MyApplicationTheme {
-        About()
-    }
+    About()
 }
 
 @Composable
 fun Subject(@DrawableRes icon: Int, title: String, body: String?) {
-    Row (modifier = Modifier.fillMaxWidth()
-            .height(100.dp)
-            .padding(vertical = 5.dp, horizontal = 16.dp)
-            .border(BorderStroke(Dp.Hairline, Color.White), shape = RoundedCornerShape(16.dp))
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        .height(100.dp)
+        .padding(vertical = 5.dp, horizontal = 16.dp)
+        .border(BorderStroke(Dp.Hairline, Color.White), shape = RoundedCornerShape(16.dp))
         , horizontalArrangement = Arrangement.spacedBy(16.dp)
     ){
         Image(
@@ -202,22 +391,23 @@ fun Subject(@DrawableRes icon: Int, title: String, body: String?) {
 @Preview(showBackground = false)
 @Composable
 fun SubjectPreview(){
-    MyApplicationTheme {
-        Subject(R.drawable.sharp_school_24, "Education", "Software Engineer")
-    }
+    Subject(R.drawable.sharp_school_24, "Education", "Software Engineer")
 }
 
 @Composable
 fun Social(){
     val context = LocalContext.current
 
-    Row (modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp)
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 10.dp)
         , horizontalArrangement = Arrangement.SpaceEvenly
     )
     { // Row for the social media icons
         Image(painter = painterResource(id = R.drawable.ic_github)
             , contentDescription = "Github Icon"
-            , modifier = Modifier.size(45.dp)
+            , modifier = Modifier
+                .size(45.dp)
                 .clickable {
                     val linkedinUrl = "https://github.com/mikeldalmauc"
                     val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedinUrl))
@@ -227,7 +417,8 @@ fun Social(){
         )
         Image(painter = painterResource(id = R.drawable.ic_linkedin)
             , contentDescription = "LinkedIn Icon"
-            , modifier = Modifier.size(45.dp)
+            , modifier = Modifier
+                .size(45.dp)
                 .clickable {
                     val linkedinUrl = "https://www.linkedin.com/in/mkldalmau/"
                     val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedinUrl))
@@ -237,7 +428,8 @@ fun Social(){
         )
         Image(painter = painterResource(id = R.drawable.ic_instagram)
             , contentDescription = "Twitter Icon"
-            , modifier = Modifier.size(45.dp)
+            , modifier = Modifier
+                .size(45.dp)
                 .clickable {
                     val linkedinUrl = "https://www.instagram.com/mikel_dalmau/"
                     val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedinUrl))
