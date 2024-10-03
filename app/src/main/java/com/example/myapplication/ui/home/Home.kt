fc2b51c7bd9df7b1c1d8fb46544acaba87ad46da
mikeldalmauc <mikeldalmauc@gmail.com>
2024-10-03 15:19:34 +0200

    colors

--- app/src/main/java/com/example/myapplication/ui/home/Home.kt
+++ app/src/main/java/com/example/myapplication/ui/home/Home.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication.ui.home
 
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,17 +10,35 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -39,6 +60,8 @@ fun HomeScreen(innerPadding: PaddingValues, navController: NavController) {
         ) {
             item {
                 Contador(homeViewModel)
+                Spacer(modifier = Modifier.height(16.dp))
+                HueSaturationValueScreen(homeViewModel)
             }
         }
     }
@@ -79,5 +102,129 @@ fun Contador(viewModel: HomeViewModel) {
                 Text(text = "-")
             }
         }
+    }
+}
+
+
+@Composable
+fun HueSaturationValueScreen(viewModel: HomeViewModel){
+    val hue: Float by viewModel.hue.observeAsState(initial = 0f)
+    val saturation: Float by viewModel.saturation.observeAsState(initial = 1.0f)
+    val value: Float by viewModel.value.observeAsState(initial = 0.5f)
+    val alpha: Float by viewModel.alpha.observeAsState(initial = 1f)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.hsv(hue, saturation, value, alpha))
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp)
+        ) {
+        }
+        Section("Hue", hue) { viewModel.onHueChanged(it) }
+    }
+}
+
+@Composable
+@Preview
+fun SectionPreview() {
+    Section(name = "Hue", value = 0f, onValueChange = {})
+}
+
+@Composable
+fun Section(name: String, value: Float, onValueChange: (Float) -> Unit){
+    Column (
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Row (horizontalArrangement = Arrangement.SpaceEvenly){
+            Text(text = "$name:")
+            OutlinedTextField(
+                value = value.toString()
+                , onValueChange = { onValueChange(it.toFloatOrNull() ?: 0f) }
+                , keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                , singleLine = true
+                , maxLines = 1
+                , modifier = Modifier.width(80.dp).height(40.dp).padding(5.dp)
+                , colors =  TextFieldDefaults.colors(
+                    disabledTextColor = MaterialTheme.colorScheme.onSurface,
+                    focusedTextColor = MaterialTheme.colorScheme.onSurface,
+                    focusedIndicatorColor = MaterialTheme.colorScheme.onSurface,
+                    unfocusedIndicatorColor = MaterialTheme.colorScheme.surfaceDim,
+                    errorIndicatorColor = MaterialTheme.colorScheme.onError
+                )
+            )
+        }
+        HueSlider(hue = value, onHueChange = { onValueChange(it) })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HueSlider(hue: Float, onHueChange: (Float) -> Unit){
+
+        // Slider sin colores para que la pista personalizada sea visible
+        Slider(
+            value = hue,
+            onValueChange = { onHueChange(it) },
+            valueRange = 0f..360f,
+            steps = 359, // Para representar correctamente los 360 tonos
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 0.dp),
+            colors = SliderDefaults.colors(
+                thumbColor = Color.White, // Color del "thumb" del slider
+                activeTrackColor = Color.Transparent, // Hacer transparente el track activo
+                inactiveTrackColor = Color.Transparent, // Hacer transparente el track inactivo
+            ),
+            track = { sliderState ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(12.dp) // Ajustar el tamaño
+                        .drawWithCache {
+                            // Crear un degradado multicolor horizontal
+                            val gradient = Brush.horizontalGradient(
+                                colors = List(361) { Color.hsv(it.toFloat(), 1f, 1f) }
+                            )
+                            onDrawBehind {
+                                // Dibujar la pista del slider como una barra multicolor
+                                drawRoundRect(
+                                    brush = gradient,
+                                    size = Size(size.width, 12.dp.toPx()), // Ajustar el grosor de la pista
+                                )
+                            }
+                        }
+                ){}
+            }
+            , // No mostrar el track por completo
+            thumb = {
+                // Dibujar el thumb personalizado como un triángulo invertido
+                CustomThumb()
+            }
+        )
+    }
+
+
+@Composable
+fun CustomThumb(){
+    Canvas(modifier = Modifier.size(18.dp)
+        .offset(x = 0.dp, y = 10.dp)) {
+        val trianglePath = Path().apply {
+            moveTo(size.width / 2f, 0f) // Punto inferior medio (punta)
+            lineTo(0f, size.height) // Lado izquierdo
+            lineTo(size.width, size.height) // Lado derecho
+            close()
+        }
+        drawPath(trianglePath, color = Color.White) // Color del triángulo
     }
 }
--- app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -8,6 +8,7 @@ import com.example.myapplication.ui.gallery.sampleArtworks
 
 class HomeViewModel : ViewModel() {
 
+    // LiveData para el contador
     private val _count = MutableLiveData<Int>()
     val count: LiveData<Int> = _count
 
@@ -17,6 +18,42 @@ class HomeViewModel : ViewModel() {
 
     fun downCount() {
         _count.value = (_count.value ?: 0) - 1
+    }
+
+    // Live Data para el Hue Saturation Value Color picker
+
+   /*
+    hue - The color value in the range (0..360), where 0 is red, 120 is green, and 240 is blue
+    saturation - The amount of hue represented in the color in the range (0..1), where 0 has no color and 1 is fully saturated.
+    alpha - Alpha channel to apply to the computed color
+    value - The strength of the color, where 0 is black.
+    colorSpace - The RGB color space used to calculate the Color from the HSV values.
+    */
+    private val _hue = MutableLiveData<Float>()
+    val hue: LiveData<Float> = _hue
+
+    private val _saturation = MutableLiveData<Float>()
+    val saturation: LiveData<Float> = _saturation
+
+    private val _value = MutableLiveData<Float>()
+    val value: LiveData<Float> = _value
+
+    private val _alpha = MutableLiveData<Float>()
+    val alpha: LiveData<Float> = _alpha
+
+    init {
+        _hue.value = 0f
+        _saturation.value = 0.7f
+        _value.value = 0.5f
+        _alpha.value = 1f
+    }
+
+
+    fun onHueChanged(it: Float) {
+        // Limitar el valor entre 0 y 360
+        val clampedValue = it.coerceIn(0f, 360f)
+
+        _hue.value = clampedValue
     }
 
 }
